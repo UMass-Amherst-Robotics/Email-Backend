7c99d36bbcd80df98cef3488968f1201b5f86d89
Andrew Tran <andtran@umass.edu>
2020-08-25 02:10:59 -0400

    Method Name fixes + formating Names of the methods had to be different even if on different routes.
    Changed formatting of email sent through contact page

--- server.py
+++ server.py
@@ -19,18 +19,19 @@ mail = Mail(app)
 CORS(app)
 
 @app.route("/contact/", methods=['POST'])
-def submitButtonPushed():
+def submitButtonPushed_Contact():
     if request.method == 'POST':
         info = request.get_json()
         if 'firstName' in info and 'lastName' in info and 'subject' in info and 'email' in info and 'message' in info:
-                msg = Message('CONTACT ' + info['subject'] + ' ' + info['firstName'] + ' ' + info['lastName'], sender="roboticsumass@gmail.com", recipients=["roboticsumass@gmail.com"])
+                msg = Message('[CONTACT] ' + info['subject'] + ' - ' +  info['email'] + ' (' + info['firstName'] + ' ' + info['lastName'] + ')', sender="roboticsumass@gmail.com", recipients=["roboticsumass@gmail.com"])
                 msg.body = info['message']
                 mail.send(msg)
                 return "Success", 200
     return "Record not found", 400
 
+
 @app.route("/apply/", methods=['POST'])
-def submitButtonPushed():
+def submitButtonPushed_Apply():
     if request.method == 'POST':
         info = request.get_json()
         if 'firstName' in info and 'lastName' in info and 'email' in info and 'questionOne' in info and 'questionTwo' in info and 'questionThree' in info and 'questionFour' in info and 'questionFive' in info:
